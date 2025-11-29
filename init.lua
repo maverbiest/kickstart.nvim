3bd5a4e00e9065403e308e87020f4c9cbd65689b
maverbiest <maxaverbiest@gmail.com>
2025-11-29 11:40:17 +0100

    Adding fugitive for git integration.

--- init.lua
+++ init.lua
@@ -1012,6 +1012,8 @@ require('lazy').setup({
     },
   },
 
+  'https://github.com/tpope/vim-fugitive',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
